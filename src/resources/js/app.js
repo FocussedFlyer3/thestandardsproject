92baeb9a5bea87a96b9b871628528dd8d48a0860
CK Wong <kvnwong0@gmail.com>
2019-08-30 15:49:27 -0400

    allow Vue components to be imported and use in blade templates

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -17,10 +17,7 @@ window.Vue = require('vue');
  * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
  */
 
-// const files = require.context('./', true, /\.vue$/i);
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
-
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+import Example from './components/ExampleComponent.vue'
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -29,5 +26,8 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  */
 
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    components: [
+        'example-component': Example
+    ]
 });
