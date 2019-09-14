502cf77e07a9355b21ee41ac684e2e856c58a88f
CK Wong <kvnwong0@gmail.com>
2019-09-13 23:00:40 -0400

    /class API supports student scores

--- src/app/Http/Controllers/ClassRoomController.php
+++ src/app/Http/Controllers/ClassRoomController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Classes;
+use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
@@ -12,16 +13,80 @@ class ClassRoomController extends Controller {
         // obtain class info
         $class = Classes::find($classID);
 
+        //get students scores
+        $scores = $this->getScores($class);
+
+        // hide redundant attributes
+        foreach($class->users as $user){
+            $user->makeHidden(['role']);
+        }
+        $data = $class;
+
+        // response JSON
         $classroom = [
             'classroom' => [
-                'details' => $class,
-                'students' => $class->users
+                'details' => $data,
+                'scores' => $scores
             ]
         ];
         $response = json_encode($classroom);
 
-        // TODO get all user (students) from this class
         return response($response, Response::HTTP_OK);
+    }
+
+    private function getScores ($class) {
+        $proeficent = 0;
+        $proeficentStudents = [];
+        $almostProeficent = 0;
+        $almostProeficentStudents = [];
+        $notProeficent = 0;
+        $notProeficentStudents = [];
+
+        // get all students scores
+        $studentScores = $class->scores->groupBy('user_id');
+
+        // compute scores
+        foreach ($studentScores as $index => $scores) {
+            $totalScore = 0;
+            $currentUser = User::find($index);
+            foreach ($scores as $score) {
+                $totalScore += $score->score;
+            }
+
+            switch (true) {
+                case $totalScore < 40.00:
+                    $notProeficentStudents[$notProeficent] = $currentUser;
+                    $notProeficent++;
+                    break;
+                
+                case $totalScore < 75.00:
+                    $almostProeficentStudents[$almostProeficent] = $currentUser;
+                    $almostProeficent++;
+                    break;
+                
+                case $totalScore > 75.00:
+                    $proeficentStudents[$proeficent] = $currentUser;
+                    $proeficent++;
+                    break;
+            }
+        }
+
+        $result = [
+            'proeficent' => [
+                'count' => $proeficent,
+                'users' => $proeficentStudents
+            ],
+            'almostProeficent' => [
+                'count' => $almostProeficent,
+                'users' => $almostProeficentStudents
+            ],
+            'notProeficent' => [
+                'count' => $notProeficent,
+                'users' => $notProeficentStudents
+            ]
+        ];
+
+        return $result;
     }
 }
 
