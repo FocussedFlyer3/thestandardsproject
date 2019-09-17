516e922a7bea77912bba52c2153e9c886602566a
CK Wong <kvnwong0@gmail.com>
2019-09-16 22:30:35 -0400

    New test cases

--- src/tests/Feature/ExampleTest.php
+++ src/tests/Feature/ExampleTest.php
@@ -4,6 +4,8 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\Request;
+use App\User;
 
 class ExampleTest extends TestCase
 {
@@ -17,5 +19,31 @@ class ExampleTest extends TestCase
         $response = $this->get('/');
 
         $response->assertStatus(200);
+    }
+    public function testLoginUser()
+    {
+        $response = $this->get('/login');
+        $response->assertSuccessful();
+        $response->assertViewIs('auth.login');
+    }
+    public function testArrayData(){
+        $data = [];
+        $this->assertEmpty($data);
+    }
+    public function testDatabaseHasEmail(){
+        $this->assertDatabaseHas('users', [
+            'email' => 'four@email.com'
+        ]);
+    }
+    public function testDatabaseHasSchool(){
+        $response = $this->assertDatabaseHas('classes', ['school' => 'PS101']);
+        $response->assertSuccessful();
+    }
+    public function testDatabaseDoesNotHave(){
+        $this->assertDatabaseMissing('proficiency', ['name' => 'Philosophy']);
+    }
+    public function testDoesNotShowView(){
+        $response = $this->get('/rail');
+        $response->assertStatus(404);
     }
 }
