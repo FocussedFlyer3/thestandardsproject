89c6e5533bc091701abd39f95cfa200d824376f5
CK Wong <kvnwong0@gmail.com>
2019-11-19 22:45:55 -0500

    new record object
    - relationship with score
    - each score id can have multiple records

---
+++ src/app/ScoreRecord.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Relations\Pivot;
+
+class ScoreRecord extends pivot
+{
+    /* 
+    |--------------------------------------------------------------------------
+    | Score-Record Pivot model
+    |--------------------------------------------------------------------------
+    |
+    | This model object serve as a pivot model to map individual 
+    | recorded module records with a score
+    |
+    */
+
+}
