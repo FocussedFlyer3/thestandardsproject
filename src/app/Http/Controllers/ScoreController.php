90d81738995f0188470f6f930edeb0ef62817707
CK Wong <kvnwong0@gmail.com>
2019-11-21 17:27:14 -0500

    Update ScoreController.php
    - now supports recording students' answers

--- src/app/Http/Controllers/ScoreController.php
+++ src/app/Http/Controllers/ScoreController.php
@@ -8,72 +8,15 @@ use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\TaskController;
 use App\Score;
 use App\User;
+use App\Record;
+use App\Task;
 use Auth;
 
 class ScoreController extends Controller
-{   
-    /**
-     * Update a current score
-     * @param $userID is the assignor id
-     * @param $request is a json body containing score for that task
-     *
-     * @return HTTP response with status
-     */
-    public function updateScore ($userID, Request $request) {
-        $data = json_decode($request->getContent(), true);
-
-        // score does not exist in POST body content
-        if (!isset($data['scoreInfo']['score_id'])) {
-            $error = [
-                'error' => [
-                    'message' => 'BAD REQUEST: could not find KEY (score_id) in json body',
-                    'code' => 400
-                ]
-            ];
-            Log::info('BAD REQUEST: json key not found \n'.json_encode($data));
-
-            return response($error, Response::HTTP_BAD_REQUEST);
-        }
-
-        $score = Score::find($data['scoreInfo']['score_id']);
-        if (!$score) {
-            $error = [
-                'error' => [
-                    'message' => 'BAD REQUEST: score id '.$data['scoreInfo']['score_id'].' do not exist!'  ,
-                    'code' => 400
-                ]
-            ];
-            Log::info('BAD REQUEST: score id do not exist\n'.json_encode($data));
-
-            return response($error, Response::HTTP_BAD_REQUEST);
-        }
-
-        try {
-            $score->score = $data['scoreInfo']['score'];
-
-        } catch (Exception $e) {
-            $error = [
-                'code' => 400,
-                'message' => 'Opps! Looks like something went wrong, try again later!'
-            ];
-            Log::info('BAD REQUEST: '.json_encode($data));
-            Log::error($e);
-
-            return response($error, Response::HTTP_BAD_REQUEST);
-        }
-        $score->save();
-        $response = [
-            'code' => 200,
-            'messsage' => 'Score successfully updated!'
-        ];
-        
-        $response = json_encode($response);
-        
-        return response($response, Response::HTTP_OK);
-    }
+{ 
 
     /**
-     * Add score to a task
+     * Add/update score to a task
      * @param $userID is the assignor id
      * @param $taskID is the task id assigned
      * @param $request is a json body containing score for that task
@@ -84,14 +27,14 @@ class ScoreController extends Controller
         $data = json_decode($request->getContent(), true);
 
         // score does not exist in POST body content
-        if (!isset($data['scoreInfo']['score']) && !isset($data['scoreInfo']['class_id'])) {
+        if (!isset($data['scoreInfo']['score']) && !isset($data['scoreInfo']['class_id']) && isset($data['scoreInfo']['records'])) {
             $error = [
                 'error' => [
-                    'message' => 'BAD REQUEST: could not find KEY (score_id) in json body',
+                    'message' => 'BAD REQUEST: could not find KEY (class_id or score or record) in json body',
                     'code' => 400
                 ]
             ];
-            Log::info('BAD REQUEST: json key not found \n'.json_encode($data));
+            Log::info('BAD REQUEST: json key not found: '.json_encode($data));
 
             return response($error, Response::HTTP_BAD_REQUEST);
         }
@@ -101,10 +44,18 @@ class ScoreController extends Controller
         $pivotData = $user->tasks()->where('task_id', $taskID)->get();
         $score_id = $pivotData[0]->pivot->score_id;
 
-        // update score if exist
+        // update existing score if exist
         if ($score_id != NULL) {
             try {
                 Score::where('score_id', $score_id)->update(['score'=> $data['scoreInfo']['score']]);
+
+                // update existing recordings
+                $score = Score::find($score_id);
+                $recordedModule = [
+                    'records' => $data['scoreInfo']['records']
+                ];
+                $score->records()->update(['module_records' => json_encode($recordedModule)]);
+     
             } catch (Exception $e){
                 $error = [
                     'code' => 400,
@@ -126,12 +77,29 @@ class ScoreController extends Controller
             return response($response, Response::HTTP_OK);
 
         } else {
-            // create & insert a score
+
             try {
+                // create & insert a score
                 $score = new Score;
                 $score['user_id'] = $userID;
                 $score['score'] = $data['scoreInfo']['score'];
                 $score['class_id'] = $data['scoreInfo']['class_id'];
+                $score->save();
+
+                // update task_user to new score id
+                // default called to this API updates status to '2' as done
+                $currentTask = Task::find($taskID);
+                $currentTask->users()->updateExistingPivot($taskID, ['score_id' => $score['score_id'], 'status' => 2]);
+
+                // insert recordings of students answers, quiz scores and game scores
+                $record = new Record;
+                $recordedModule = [
+                    'records' => $data['scoreInfo']['records']
+                ];
+                $record['module_records'] = json_encode($recordedModule);
+                $record->save();
+
+                $record->scores()->attach($score['score_id'], [ 'record_id' => $record['id']]);    
     
             } catch (Exception $e) {
                 $error = [
@@ -140,25 +108,6 @@ class ScoreController extends Controller
                 ];
                 Log::info('BAD REQUEST: '.json_encode($data));
                 Log::error($e);
-    
-                return response($error, Response::HTTP_BAD_REQUEST);
-            }
-            $score->save();
-    
-            // update score_id in task_user table
-            try {
-                $user->tasks()->where('task_id', $taskID)->update(['score_id' => $score->score_id]);
-    
-            } catch (Exception $e) {
-                $error = [
-                    'code' => 400,
-                    'message' => 'Opps! Looks like something went wrong, try again later!'
-                ];
-                Log::info('BAD REQUEST: '.json_encode($data));
-                Log::error($e);
-    
-                // remove insert score
-                Score::find($score->score_id)->delete();
     
                 return response($error, Response::HTTP_BAD_REQUEST);
             }
